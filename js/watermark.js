51102d30a65a057e68956c21287db76d2cc6f2ea
Rainycrying <admin@rainycrying.com>
2024-01-10 16:35:31 +0800

    Update watermark.js
    Signed-off-by: Rainycrying <admin@rainycrying.com>

--- js/watermark.js
+++ js/watermark.js
@@ -19,38 +19,39 @@ function watermark(settings) {
     };
 
     const mergedSettings = { ...defaultSettings, ...settings };
+    const { watermark_cols, watermark_rows, watermark_x, watermark_y, watermark_width, watermark_height, watermark_x_space, watermark_y_space, watermark_angle } = mergedSettings;
+    const pageWidth = screen.width;
+    const pageHeight = screen.height;
 
-    const page_width = screen.width;
-    const page_height = screen.height;
-
-    if (mergedSettings.watermark_cols === 0 || (mergedSettings.watermark_x + mergedSettings.watermark_width * mergedSettings.watermark_cols + mergedSettings.watermark_x_space * (mergedSettings.watermark_cols - 1)) < page_width) {
-        mergedSettings.watermark_cols = parseInt((page_width - mergedSettings.watermark_x + mergedSettings.watermark_x_space) / (mergedSettings.watermark_width + mergedSettings.watermark_x_space));
-        mergedSettings.watermark_x_space = parseInt((page_width - mergedSettings.watermark_x - mergedSettings.watermark_width * mergedSettings.watermark_cols) / (mergedSettings.watermark_cols - 1));
+    if (watermark_cols === 0 || (parseInt(watermark_x + watermark_width * watermark_cols + watermark_x_space * (watermark_cols - 1)) < pageWidth)) {
+        mergedSettings.watermark_cols = parseInt((pageWidth - watermark_x + watermark_x_space) / (watermark_width + watermark_x_space));
+        mergedSettings.watermark_x_space = parseInt((pageWidth - watermark_x - watermark_width * watermark_cols) / (watermark_cols - 1));
     }
 
-    if (mergedSettings.watermark_rows === 0 || (mergedSettings.watermark_y + mergedSettings.watermark_height * mergedSettings.watermark_rows + mergedSettings.watermark_y_space * (mergedSettings.watermark_rows - 1)) < page_height) {
-        mergedSettings.watermark_rows = parseInt((mergedSettings.watermark_y_space + page_height - mergedSettings.watermark_y) / (mergedSettings.watermark_height + mergedSettings.watermark_y_space));
-        mergedSettings.watermark_y_space = parseInt(((page_height - mergedSettings.watermark_y) - mergedSettings.watermark_height * mergedSettings.watermark_rows) / (mergedSettings.watermark_rows - 1));
+    if (watermark_rows === 0 || (parseInt(watermark_y + watermark_height * watermark_rows + watermark_y_space * (watermark_rows - 1)) < pageHeight)) {
+        mergedSettings.watermark_rows = parseInt((watermark_y_space + pageHeight - watermark_y) / (watermark_height + watermark_y_space));
+        mergedSettings.watermark_y_space = parseInt(((pageHeight - watermark_y) - watermark_height * watermark_rows) / (watermark_rows - 1));
     }
 
     const oTemp = document.createDocumentFragment();
 
-    for (let i = 0; i < mergedSettings.watermark_rows; i++) {
-        const y = mergedSettings.watermark_y + (mergedSettings.watermark_y_space + mergedSettings.watermark_height) * i;
+    for (let i = 0; i < watermark_rows; i++) {
+        const y = watermark_y + (watermark_y_space + watermark_height) * i;
 
-        for (let j = 0; j < mergedSettings.watermark_cols; j++) {
-            const x = mergedSettings.watermark_x + (mergedSettings.watermark_width + mergedSettings.watermark_x_space) * j;
+        for (let j = 0; j < watermark_cols; j++) {
+            const x = watermark_x + (watermark_width + watermark_x_space) * j;
 
             const mask_div = document.createElement('div');
             mask_div.id = `mask_div${i}${j}`;
             mask_div.appendChild(document.createTextNode(mergedSettings.watermark_txt));
+            mask_div.classList.add('watermark');
 
             mask_div.style.cssText = `
-                -webkit-transform: rotate(-${mergedSettings.watermark_angle}deg);
-                -moz-transform: rotate(-${mergedSettings.watermark_angle}deg);
-                -ms-transform: rotate(-${mergedSettings.watermark_angle}deg);
-                -o-transform: rotate(-${mergedSettings.watermark_angle}deg);
-                transform: rotate(-${mergedSettings.watermark_angle}deg);
+                -webkit-transform: rotate(-${watermark_angle}deg);
+                -moz-transform: rotate(-${watermark_angle}deg);
+                -ms-transform: rotate(-${watermark_angle}deg);
+                -o-transform: rotate(-${watermark_angle}deg);
+                transform: rotate(-${watermark_angle}deg);
                 visibility: visible;
                 position: fixed;
                 left: ${x}px;
@@ -74,19 +75,51 @@ function watermark(settings) {
     document.body.appendChild(oTemp);
 }
 
-function mainusage() {
-    const url = `${document.location.protocol}//${document.location.hostname}:${document.location.port}/rest/api/user/current`;
+function getIP(callback) {
     const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://ip.rainycrying.com/", true);
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            const username = JSON.parse(xhr.responseText).username;
-            watermark({ watermark_txt: username });
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const response = xhr.responseText;
+            const ip = response;
+            callback(ip);
+        } else {
+            callback(null);
         }
     };
 
-    xhr.open("GET", url, true);
-    xhr.send(null);
+    xhr.onerror = function () {
+        callback(null);
+    };
+
+    xhr.send();
 }
 
-mainusage();
+function generateWatermark() {
+    const timestamp = new Date().getTime();
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const formattedDate = `${year}-${month}-${day}`;
+    const domain = window.location.hostname;
+
+    getIP(function (ip) {
+        const output = `${domain}_${formattedDate}_${ip || 'unknown'}`;
+
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            const [name, value] = cookie.split('=');
+            if (name === "cookieName") {
+                watermark({ watermark_txt: value });
+                return;
+            }
+        }
+
+        watermark({ watermark_txt: output });
+    });
+}
+
+generateWatermark();
